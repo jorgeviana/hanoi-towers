1b127c563f305b429280abaa97099d84d03e0cbe
Jorge Viana <jorgeviana@users.noreply.github.com>
2023-06-27 09:44:49 +0200

    rust: 1 disk impl

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,10 @@ pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
+pub fn hanoi(n: usize) ->  &'static str {
+    "A to C"
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -10,5 +14,11 @@ mod tests {
     fn it_works() {
         let result = add(2, 2);
         assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn should_solve_hanoi_with_1_disk() {
+        let result = hanoi(1);
+        assert_eq!(result, "A to C")
     }
 }
