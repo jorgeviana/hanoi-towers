af9e81d9889d5c05b48bd1e80deab27c5f20b2d5
Jorge Viana <jorgeviana@users.noreply.github.com>
2023-06-27 10:05:39 +0200

    rust: 2 disks

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
 pub fn hanoi(n: usize) ->  &'static str {
-    "A to C"
+    if n == 1 {
+        "A to C"
+    } else {
+        "A to B\nA to C\nB to C"
+    }
 }
 
 #[cfg(test)]
@@ -10,5 +14,11 @@ mod tests {
     fn should_solve_hanoi_with_1_disk() {
         let result = hanoi(1);
         assert_eq!(result, "A to C")
+    }
+
+    #[test]
+    fn should_solve_hanoi_with_2_disks() {
+        let result = hanoi(2);
+        assert_eq!(result, "A to B\nA to C\nB to C")
     }
 }
